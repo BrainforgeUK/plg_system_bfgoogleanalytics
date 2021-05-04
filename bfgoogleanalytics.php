71d8e64f0600a2dd42219894df9d763ce0d6e7de
Brainforge.UK <brainforge.uk@btinternet.com>
2021-05-04 16:03:03 +0100

    Rewritten.

--- bfgoogleanalytics.php
+++ bfgoogleanalytics.php
@@ -2,45 +2,76 @@
 /**
  * @package   Plugin for adding Google Analytics to site.
  * @version   0.0.1
- * @author    http://www.brainforge.co.uk
- * @copyright Copyright (C) 2011-2018 Jonathan Brain. All rights reserved.
+ * @author    https://www.brainforge.co.uk
+ * @copyright Copyright (C) 2011-2021 Jonathan Brain. All rights reserved.
  * @license	 GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
+
+use Joomla\CMS\Plugin\CMSPlugin;
 
 // no direct access
 defined('_JEXEC') or die;
 
-jimport( 'joomla.plugin.plugin');
-
-class plgSystemBFGoogleAnalytics extends JPlugin
+class plgSystemBFGoogleAnalytics extends CMSPlugin
 {
-	private $trackingcode;
+	protected $application;
+	protected $trackingcodes;
+	protected $measurementID0;
 
+	/*
+	 */
 	function __construct(&$subject, $config)
 	{
 		parent::__construct($subject, $config);
-	}
 
-	function onBeforeRender()
-	{
-		$this->trackingcode = trim($this->params->get('trackingcode'));
-		if (empty($this->trackingcode) || strpos($this->trackingcode, 'UA-00000000') !== false)
+		$this->application = JFactory::getApplication();
+
+		$this->trackingcodes = (array)$this->params->get('trackingcodes', array());
+		if (!empty($this->trackingcodes))
 		{
-			$application = JFactory::getApplication();
-			if($application->isAdmin())
+			$this->measurementID0 = trim(reset($this->trackingcodes)->measurementid);
+			if (!preg_match('/^UA-[0-9]{8}-[0-9]$/', $this->measurementID0))
 			{
-				JFactory::getLanguage()->load('plg_system_bfgoogleanalytics');
-				$application->enqueueMessage(JText::_('PLG_BFGOOGLEANALYTICS_TRACKINGCODE_ERROR'), 'warning');
+				$this->measurementID0 = null;
 			}
-			$this->trackingcode = null;
-			return true;
 		}
 	}
 
+	/*
+	 */
+	function onBeforeRender()
+	{
+
+
+		if (empty($this->measurementID0) || $this->measurementID0 == 'UA-00000000-0')
+		{
+			if($this->application->isClient('administrator'))
+			{
+				$this->application->getLanguage()->load('plg_system_bfgoogleanalytics');
+				$this->application->enqueueMessage(JText::_('PLG_BFGOOGLEANALYTICS_TRACKINGCODE_ERROR'), 'warning');
+			}
+			$this->trackingcodes = null;
+		}
+	}
+
+	/*
+	 */
 	function onAfterRender()
 	{
-		if (empty($this->trackingcode)) {
-			return true;
+		if (empty($this->measurementID0)) {
+			return;
+		}
+
+		if ($this->application->isClient('administrator'))
+		{
+			if ($this->params->get('showInAdmin') != '1')
+			{
+				return;
+			}
+		}
+		else if (!$this->application->isClient('site'))
+		{
+			return;
 		}
 
 		if (!empty($_SERVER['SERVER_ADDR']) &&
@@ -48,41 +79,49 @@ class plgSystemBFGoogleAnalytics extends JPlugin
 		{
 			if ($_SERVER['SERVER_ADDR'] == @$_SERVER['REMOTE_ADDR'])
 			{
-				return true;
+				return;
 			}
 			if (substr($_SERVER["SERVER_ADDR"], 0, 2) == 'fd' ||
 				substr($_SERVER["SERVER_ADDR"], 0, 2) == 'fe')
 			{
-				return true;
+				return;
 			}
 			$baseIP = explode('.', $_SERVER["SERVER_ADDR"]);
 			switch ($baseIP[0])
 			{
-				case '127': return true;
+				case '127': return;
 				case '192':
-					if ($baseIP[1] == '168') return true;
+					if ($baseIP[1] == '168') return;
 					break;
 			}
 		}
 
-		if ($this->params->get('showInAdmin') != '1')
+		$gaCode = '
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=' . $this->measurementID0 . '"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+';
+
+		foreach($this->trackingcodes as $trackingcode)
 		{
-			if(JFactory::getApplication()->isAdmin()) return true;
+			$gaCode .= "  gtag('config', '" . trim($trackingcode->measurementid) . "');\n";
 		}
 
-		$this->buffer = JResponse::getBody();
-		$pos = stripos($this->buffer, '<head');
-		if ($pos === false) return false;
-		$pos = stripos($this->buffer, '>', $pos);
-		if ($pos === false) return false;
+		$gaCode .= "</script>\n";
+
+		$buffer = $this->application->getBody();
+
+		$pos = stripos($buffer, '<head');
+		if ($pos === false) return;
+		$pos = stripos($buffer, '>', $pos);
+		if ($pos === false) return;
 		$pos += 1;
 
-		$this->buffer = substr($this->buffer, 0, $pos) .
-			$this->trackingcode .
-			substr($this->buffer, $pos);
-		JResponse::setBody($this->buffer);
+		$buffer = substr($buffer, 0, $pos) . $gaCode . substr($buffer, $pos);
 
-		return true;
+		$this->application->setBody($buffer);
 	}
 }
 ?>
